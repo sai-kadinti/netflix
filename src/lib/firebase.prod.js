54c45504a54f7c7a4d529384f2906d52be1f9eba
sai-kadinti <85397181+sai-kadinti@users.noreply.github.com>
2025-07-24 22:53:16 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,23 +1,21 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { seedDatabase } from '../seed';
 
-//we need to somehow seed the database
+// Removed: import { seedDatabase } from '../seed';
 
-//we need a config here, put your api details here 
 const config = {
-    apiKey: "AIzaSyD6rfFIRNC4j0YoRtZGi6vLlJ0M2baoffE",
-    authDomain: "netflix-react-9480e.firebaseapp.com",
-    projectId: "netflix-react-9480e",
-    storageBucket: "netflix-react-9480e.appspot.com",
-    messagingSenderId: "124358470674",
-    appId: "1:124358470674:web:e265ab9bc66f7c4e9a3ccb"
+  apiKey: "AIzaSyD6rfFIRNC4j0YoRtZGi6vLlJ0M2baoffE",
+  authDomain: "netflix-react-9480e.firebaseapp.com",
+  projectId: "netflix-react-9480e",
+  storageBucket: "netflix-react-9480e.appspot.com",
+  messagingSenderId: "124358470674",
+  appId: "1:124358470674:web:e265ab9bc66f7c4e9a3ccb"
 };
 
 const firebase = Firebase.initializeApp(config);
 
-//dont uncomment this bcz it will dupliacte the database on firebase
-//seedDatabase(firebase);
+// Don't uncomment this unless you need to reseed your Firebase manually.
+// seedDatabase(firebase);
 
 export { firebase };
