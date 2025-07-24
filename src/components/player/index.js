0c56a241642c0d503d78be8b47e809918f9a7bb4
sai-kadinti <85397181+sai-kadinti@users.noreply.github.com>
2025-07-24 22:46:30 +0530

    Update index.js

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -8,32 +8,35 @@ export default function Player({ children, ...restProps }) {
     const [showPlayer, setShowPlayer] = useState(false);
 
     return (
-        <PlayerContext.Provider value={{ showPlayer, setShowPlayer}}>
+        <PlayerContext.Provider value={{ showPlayer, setShowPlayer }}>
             <Container {...restProps}>{children}</Container>
         </PlayerContext.Provider>
     );
 }
 
-Player.Video = function PlayerVideo({ src, ...restProps}) {
-    const{ showPlayer, setShowPlayer } = useContext(PlayerContext);
+Player.Video = function PlayerVideo({ src, ...restProps }) {
+    const { showPlayer, setShowPlayer } = useContext(PlayerContext);
 
-    return showPlayer ? ReactDOM.createPortal (
-        <Overlay onClick={() => setShowPlayer(false)} {...restProps}>
-            <Inner>
-                <video id="netflix-player" controls>
-                    <source src={src} type="video/mp4" />
-                </video>
-                <Close />
-            </Inner>
-        </Overlay>,
-        document.body
-    ) : null;
+    return showPlayer
+        ? ReactDOM.createPortal(
+              <Overlay onClick={() => setShowPlayer(false)} {...restProps}>
+                  <Inner>
+                      <video id="netflix-player" controls>
+                          <source src={src} type="video/mp4" />
+                      </video>
+                      <Close />
+                  </Inner>
+              </Overlay>,
+              document.body
+          )
+        : null;
 };
 
-Player.Button = function PlayerBUtton({...restProps}) {
-    const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+Player.Button = function PlayerButton({ ...restProps }) {
+    const { setShowPlayer } = useContext(PlayerContext); // Removed unused showPlayer
+
     return (
-        <Button onClick={() => setShowPlayer((showPlayer) => !showPlayer)}>
+        <Button onClick={() => setShowPlayer((show) => !show)} {...restProps}>
             Play
         </Button>
     );
